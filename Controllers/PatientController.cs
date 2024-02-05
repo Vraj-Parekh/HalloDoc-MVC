d9142c620c7552005006bbf5b9db481ebe4a95e6
VrajParekh <vrajparekh58@gmail.com>
2024-02-05 18:30:35 +0530

    Add project files.

---
+++ Controllers/PatientController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDoc_Project.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult PatientLogin() {
+            return View();
+        }
+        public IActionResult ResetPwd()
+        {
+            return View();
+        }
+        public IActionResult SubmitRequest()
+        {
+            return View();
+        }
+        public IActionResult PatientInfo()
+        {
+            return View();
+        } public IActionResult FamilyInfo()
+        {
+            return View();
+        } public IActionResult ConciergeInfo()
+        {
+            return View();
+        } public IActionResult BusinessInfo()
+        {
+            return View();
+        }
+        
+    }
+}
